531bb7791a5c3bdd1a1e580f26813e17a4f1570b
spinosaphb <spinosaphb@gmail.com>
2022-05-14 08:16:41 -0300

    Adicionando atividades de classe

---
+++ class-activities/activity_1/code5.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import re
+
+import os
+import sys
+sys.path.append(os.getcwd().split('/src')[0])
+
+from nltk.tokenize import regexp_tokenize
+from src.nlp_utils import get_sample_Santo_Graal
+
+# Split the script into lines: lines
+holy_grail = get_sample_Santo_Graal()
+lines = holy_grail.split('\n')
+
+# Replace all script lines for speaker
+pattern = "[A-Z]{2,}(\s)?(#\d)?([A-Z]{2,})?:"
+lines = [re.sub(pattern, ' ', l) for l in lines]
+
+# Tokenize each line: tokenized_lines
+tokenized_lines = [regexp_tokenize(s,r'\w+') for s in lines]
+
+# Make a frequency list of lengths: line_num_words
+line_num_words = [len(t_line) for t_line in tokenized_lines]
+
+# Plot a histogram of the line lengths
+plt.hist(line_num_words)
+
+# Show the plot
+plt.show()
